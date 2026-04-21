16792e823d7a5a080384f627f9f5f5a846f901d6
MovelikeLo <loicrobert640@gmail.com>
2026-04-21 18:32:09 -0400

    Change zIndex when clicked once

--- squares.js
+++ squares.js
@@ -1,6 +1,6 @@
 let squareArea = document.querySelector("#squarearea");
 let squareCount = parseInt(Math.random()*21)+ 30;
-
+let zIndex = 1000;
 for (let i = 0; i < squareCount; i++) {
     addSquare();
 }
@@ -22,6 +22,7 @@ function addSquare() {
     square.style.left = parseInt(Math.random() * 650) + "px";
     square.style.backgroundColor = getRandomColor();
     squareArea.appendChild(square);
+    square.onclick = changeZIndex;
 }
 
 function changeColors() {
@@ -33,4 +34,9 @@ function changeColors() {
     squares.forEach(square => {
         square.style.backgroundColor = getRandomColor();
     });
+}
+function changeZIndex() {
+    zIndex++;
+    this.style.zIndex = zIndex;
+    
 }
